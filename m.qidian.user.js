dc165575bdff5cc9a686315c7c904b8e4cef7196
cxumol <cxumol@users.noreply.github.com>
2021-12-18 04:01:54 -0800

    Update m.qidian.user.js

--- m.qidian.user.js
+++ m.qidian.user.js
@@ -2,7 +2,7 @@
 // @name         起点手机站跳转电脑版书页
 // @namespace    https://github.com/cxumol/userscripts/
 // @version      0.1
-// @description  谷歌收录起点书, 老爱收录其手机网页版网址, 给电脑用户带来的不便, 用此脚本自动跳转
+// @description  谷歌收录起点书, 老爱收录其手机网页版网址, 给电脑用户带来的不便, 用此脚本自动跳转, 从 m.qidian.com/book/*.html 转到 book.qidian.com/info/*/
 // @author       cxumol
 // @match        https://m.qidian.com/book/*.html
 // @icon         https://www.google.com/s2/favicons?domain=qidian.com
